f4f5ce67316274cd08f9d74bf0f82ac0484fed53
eguo <eguo.opysky@gmail.com>
2018-05-07 22:24:36 +0900

    add ApplicationMenu

--- youtube-player/src/main.ts
+++ youtube-player/src/main.ts
@@ -1,14 +1,37 @@
-import {app, BrowserWindow} from 'electron';
+import {app, BrowserWindow, Menu, MenuItemConstructorOptions} from 'electron';
 
 let win: BrowserWindow;
 
 app.on('ready', () => {
     win = new BrowserWindow({width: 800, height: 600});
+
     win.loadURL(`file://${__dirname}/../index.html`);
-    win.webContents.openDevTools();
+    
     win.on('closed', () => {
         win = null;
     });
+
+    const menuTemplate: MenuItemConstructorOptions[] = [
+        {
+            label: 'File(&F)', 
+            submenu: [
+                {label: 'Exit(&X)', click: app.quit}
+            ]
+        }, 
+        {
+            label: 'Edit(&E)', 
+            submenu: [
+                {label: 'Refresh(&R)', accelerator: 'CmdOrCtrl+R', click: () => win.webContents.reload()}
+            ]
+        }, 
+        {
+            label: 'Tools(&T)', 
+            submenu: [
+                {label: 'Toggle Developer Tools', accelerator: 'F12', click: () => win.webContents.toggleDevTools()}
+            ]
+        }
+    ];
+    Menu.setApplicationMenu(Menu.buildFromTemplate(menuTemplate));
 });
 
 app.on('window-all-closed', () => {
