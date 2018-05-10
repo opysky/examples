99d4fbab14272629fdbedba7c628eb43841b701e
eguo <eguo.opysky@gmail.com>
2018-05-10 11:20:25 +0900

    Switch to /SUBSYSTEM:WINDOWS

--- CppwinrtHello/EdgeView32/main.cpp
+++ CppwinrtHello/EdgeView32/main.cpp
@@ -3,9 +3,14 @@
 using namespace winrt;
 using namespace Windows::Foundation;
 
-int main()
-{
-    init_apartment();
-    Uri uri(L"http://aka.ms/cppwinrt");
-    printf("Hello, %ls!\n", uri.AbsoluteUri().c_str());
+int APIENTRY wWinMain(HINSTANCE, HINSTANCE, LPWSTR, int) {
+    init_apartment(apartment_type::single_threaded);
+    
+	Uri uri(L"http://aka.ms/cppwinrt");
+	
+	std::array<wchar_t, 256> text;
+	std::swprintf(&text[0], text.size(), L"Hello, %ls!\n", uri.AbsoluteUri().c_str());
+	OutputDebugStringW(&text[0]);
+
+	return 0;
 }
